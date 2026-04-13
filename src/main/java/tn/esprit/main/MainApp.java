0579c9a28d29cd6a3a0de03d36ace94dd95edc3c
Wiem <wiem.anabtaoui@esprit.tn>
2026-04-13 18:36:25 +0200

    fix: make tn.esprit MainApp launch org.example app

--- src/main/java/tn/esprit/main/MainApp.java
+++ src/main/java/tn/esprit/main/MainApp.java
@@ -1,11 +1,8 @@
 package tn.esprit.main;
 
-import javafx.application.Application;
-import org.example.Main;
-
 public class MainApp {
 
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        org.example.Main.main(args);
     }
 }
