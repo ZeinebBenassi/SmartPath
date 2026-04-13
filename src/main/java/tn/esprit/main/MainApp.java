5c8fb090d375779ea99f58f585b42a79c2e7c1d5
Wiem <wiem.anabtaoui@esprit.tn>
2026-04-13 18:48:06 +0200

    Revert "fix: make tn.esprit MainApp launch org.example app"
    This reverts commit 0579c9a28d29cd6a3a0de03d36ace94dd95edc3c.

--- src/main/java/tn/esprit/main/MainApp.java
+++ src/main/java/tn/esprit/main/MainApp.java
@@ -1,8 +1,11 @@
 package tn.esprit.main;
 
+import javafx.application.Application;
+import org.example.Main;
+
 public class MainApp {
 
     public static void main(String[] args) {
-        org.example.Main.main(args);
+        Application.launch(Main.class, args);
     }
 }
